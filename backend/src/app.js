8382e1fa5c39259a4e2e0b28fb802ad1a41bb41f
Trisha Mae A. Hechenagocia <tahechenagocia@up.edu.ph>
2026-05-09 18:16:24 +0800

    added router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,24 @@
+
+//IMPORTS
+// -- import third-party mod --
 const express = require("express");
+// -- import built-in mod --
 const path = require("path");
+// import local mod --
+const userRouter = require('./routes/user');
+const adminRouter = require('./routes/admin');
+const orgRouter = require('./routes/org');
 
-const app = express();
+const app = express(); 
+app.use(express.json()); //translates req into json format
 
-app.use(express.json());
+//load router mod to app
+app.use('/user', userRouter);
+app.use('/admin', adminRouter);
+app.use('/org', orgRouter);
+
+
+
 
 // serve frontend files
 app.use(express.static(path.join(__dirname, "../../frontend")));
