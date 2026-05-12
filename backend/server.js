0351f1cc9a515c315efeb6c55172fbb59d16960f
Trisha Mae A. Hechenagocia <tahechenagocia@up.edu.ph>
2026-05-13 03:43:07 +0800

    added sign up and login- modified routers

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,7 +1,11 @@
 const {Router} = require('express');
 const router = Router();
 
+const db = require('../config/db'); // db connect
+const userController = require('../../controllers/userController');
 
+router.post('/signup', userController.signup); 
+router.post('/login', userController.login); 
 
 
 
