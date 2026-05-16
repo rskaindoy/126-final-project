80f247fdf05abdb55e3d0291c5e662f5063e7eb7
Trisha Mae A. Hechenagocia <tahechenagocia@up.edu.ph>
2026-05-16 16:46:05 +0800

    added in sponsorontroller

--- backend/controllers/sponsorController.js
+++ backend/controllers/sponsorController.js
@@ -5,38 +5,46 @@ const db = require('../src/config/db');
 exports.submitSponsorship = async (req, res) => {
     try {
         const { 
-            sponsor_type, 
-            entity_name, 
-            repr_name, 
-            contact_number, 
-            fb_link_1, 
-            fb_link_2, 
-            target_pet, 
-            sponsorship_month, 
+            sponsorType, 
+            entityName, 
+            contactNumber, 
+            fbLink1, 
+            fbLink2, 
+            targetPet, 
+            sponsorshipMonth, 
             amount 
         } = req.body;
         //
-        const proof_img = req.file ? req.file.filename : null;
+        if (!req.file) {
+            return res.status(400).json({ message: "Proof of payment is required." });
+        }
+        const proof_img = req.file.filename;
 
-        //status: default lng
+        //put all pets selected in one string
+        let collecPets = ""; 
+        if (Array.isArray(targetPet)) {
+            collecPets = targetPet.join(', '); 
+        } else {
+            collecPets = targetPet || ""; 
+        }
+
+
         const sql = `
             INSERT INTO sponsorship (
-                user_id, sponsor_type, entity_name, repr_name, 
+                user_id, sponsor_type, entity_name, 
                 contact_number, fb_link_1, fb_link_2, 
-                animal_id, target_month, amount, proof_img
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
+                target_pets, sponsorship_month, amount, proof_img
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
         await db.query(sql, [
-            req.session.userId,
-            sponsor_type,
-            entity_name || null, 
-            repr_name || null,
-            contact_number,
-            fb_link_1,
-            fb_link_2 || null,
-            target_pet, 
-            sponsorship_month,
+            req.session.userId,              
+            sponsorType,
+            entityName || null,              
+            contactNumber,
+            fbLink1 || null,
+            fbLink2 || null,
+            collecPets,                   
+            sponsorshipMonth,
             amount,
             proof_img
         ]);
